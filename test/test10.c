49a1f02a329308ac5a2f27327215f793cbbb22c6
marin9 <marin.bralic09@gmail.com>
2019-11-14 19:02:55 +0100

    Update

--- test/test10.c
+++ test/test10.c
@@ -8,8 +8,8 @@
 //	producer and consumer
 
 #define CONSUMERS	2
-#define PRODUCERS	3
-#define BUFF_SIZE	5
+#define PRODUCERS	2
+#define BUFF_SIZE	3
 
 #define SEM_EMPTY	0
 #define SEM_FULL	1
@@ -22,20 +22,22 @@ static int in, out;
 static int buffer[BUFF_SIZE];
 
 
-static void printf(char *f, ...){
-	char buff[64];
-	vssprintf(buff, &f);
+void printf(char *format, ...){
+	char buff[32];
+	vssprintf(buff, &format);
 	uart_print(buff);
 }
 
 
 static void consumers(void *args){
+	char buff[32];
 	while(1){
 		os_sem_take(SEM_FULL);
 		os_sem_take(SEM_CRITIC1);
 
-		printf("Consumer: %d, Data: %d\r\n",
+		sprintf(buff, "C< [%d] %d\r\n",
 				(int)args, buffer[out]);
+		uart_print(buff);
 
 		out += 1;
 		if(out>=BUFF_SIZE)
@@ -48,12 +50,16 @@ static void consumers(void *args){
 }
 
 static void producers(void *args){
+	char buff[32];
+
 	while(1){
 		os_sem_take(SEM_EMPTY);
 		os_sem_take(SEM_CRITIC2);
 
-		printf("Produces: %d, Data: %d\r\n",
+		buffer[in]=n++;
+		sprintf(buff, "P> [%d] %d\r\n",
 				(int)args, buffer[in]);
+		uart_print(buff);
 
 		in += 1;
 		if(in>=BUFF_SIZE)
@@ -61,17 +67,16 @@ static void producers(void *args){
 
 		os_sem_give(SEM_CRITIC2);
 		os_sem_give(SEM_FULL);
-		os_task_sleep(1000);
+		os_task_sleep(300);
 	}
 }
 
 
 void test10(){
 	int i;
-	time_delay(5000000);
-	uart_print("Start\r\n");
+	time_delay(6000000);
 
-	n=48;
+	n=100;
 	in=out=0;
 
 	os_sem_init(SEM_EMPTY, BUFF_SIZE);
@@ -79,10 +84,11 @@ void test10(){
 	os_sem_init(SEM_CRITIC1, 1);
 	os_sem_init(SEM_CRITIC2, 1);
 
+
 	for(i=0;i<CONSUMERS;++i)
-		os_task_create(consumers, (void*)(i+1), 1, 1024, 0);
+		os_task_create(consumers, (void*)(i+1), 1, 2*1024, 0);
 
 	for(i=0;i<PRODUCERS;++i)
-		os_task_create(producers, (void*)(i+1), 1, 1024, 0);
+		os_task_create(producers, (void*)(i+1), 1, 4*1024, 0);
 
 }
